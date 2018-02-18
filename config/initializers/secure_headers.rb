bf4a128637d96567f3d4bc7f0a5b85e14f1073dd
Nando Vieira <fnando.vieira@gmail.com>
2018-02-18 12:05:36 -0800

    Add analytics.

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -4,11 +4,22 @@ Rails.env.on(:development) do
   SecureHeaders::Configuration.default do |config|
     config.csp[:default_src] = %w['self']
     config.csp[:script_src] = %w['self' 'unsafe-eval']
+    config.csp[:script_src] = %w[
+      'self'
+      'unsafe-eval'
+      'unsafe-inline'
+      https://www.googletagmanager.com
+    ]
   end
 end
 
 Rails.env.on(:test, :production) do
   SecureHeaders::Configuration.default do |config|
     config.csp[:default_src] = %w['self']
+    config.csp[:script_src] = %w[
+      'self'
+      'unsafe-inline'
+      https://www.googletagmanager.com
+    ]
   end
 end
