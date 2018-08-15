57a33a1d65f4242328c70c43dc6bd8688afefdf1
Nando Vieira <fnando.vieira@gmail.com>
2018-08-14 20:24:23 -0700

    Use stellar.expert again.

--- app/models/wallet_url.rb
+++ app/models/wallet_url.rb
@@ -1,10 +1,10 @@
 module WalletUrl
   HOSTS = {
-    "public" => "https://steexp.com",
-    "testnet" => "https://testnet.steexp.com"
+    "public" => "https://stellar.expert/explorer/public/account",
+    "testnet" => "https://stellar.expert/explorer/testnet/account"
   }
 
   def self.get(account)
-    "#{HOSTS.fetch(Horizon.env)}/account/#{account}#payments"
+    "#{HOSTS.fetch(Horizon.env)}/#{account}"
   end
 end
